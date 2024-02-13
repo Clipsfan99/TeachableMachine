839a6b61def62bb4350446bfd2cfc1eabe6b32ba
Clipsfan99 <155583837+Clipsfan99@users.noreply.github.com>
2024-02-12 19:34:13 -0800

    finalTeachableMachineProject

--- static/sketch.js
+++ static/sketch.js
@@ -20,51 +20,69 @@ let flippedVideo;
 // To store the classification
 let label = "";
 
+let videoIsOn = false;
+
+
 // Load the model first
 function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 }
 
 function setup() {
-  createCanvas(320, 260);
-  // Create the video
-  video = createCapture(VIDEO);
-  video.size(320, 240);
-  video.hide();
+    var canvas = createCanvas(450, 358);
+    canvas.parent('sketch-holder');
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(450, 338);
+    video.hide();
 
-  flippedVideo = ml5.flipImage(video)
-  // Start classifying
-  classifyVideo();
+    flippedVideo = ml5.flipImage(video)
+    // Start classifying
+    classifyVideo();
 }
 
 function draw() {
-  background(0);
-  // Draw the video
-  image(flippedVideo, 0, 0);
+    background(0);
+    // Draw the video
+    if (videoIsOn) {
+        image(flippedVideo, 0, 0);
 
-  // Draw the label
-  fill(255);
-  textSize(16);
-  textAlign(CENTER);
-  text(label, width / 2, height - 4);
+        // Draw the label
+        fill(255);
+        textSize(16);
+        textAlign(CENTER);
+        text(label, width / 2, height - 4);
+    }
+    else{
+        background(50);
+        fill(255);
+        textSize(16);
+        textAlign(CENTER);
+        text("Click to start video", width/2, height/2);
+    }
 }
 
+function mousePressed(){
+    videoIsOn = !videoIsOn;
+    if(videoIsOn) video.play();
+    else video.stop();
+}
 // Get a prediction for the current video frame
-function classifyVideo() {
-  flippedVideo = ml5.flipImage(video)
-  classifier.classify(flippedVideo, gotResult);
-}
+    function classifyVideo() {
+        flippedVideo = ml5.flipImage(video)
+        classifier.classify(flippedVideo, gotResult);
+    }
 
 // When we get a result
-function gotResult(error, results) {
-  // If there is an error
-  if (error) {
-    console.error(error);
-    return;
-  }
-  // The results are in an array ordered by confidence.
-  // console.log(results[0]);
-  label = results[0].label;
-  // Classifiy again!
-  classifyVideo();
-}
+    function gotResult(error, results) {
+        // If there is an error
+        if (error) {
+            console.error(error);
+            return;
+        }
+        // The results are in an array ordered by confidence.
+        // console.log(results[0]);
+        label = results[0].label;
+        // Classifiy again!
+        classifyVideo();
+    }
